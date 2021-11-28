09e11c9a8c7fa6a456478fb40429a303d003d74e
Victoria Iurina <Victoria_iurina@epam.com>
2021-11-29 00:57:57 +0300

    Fixed truncate task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,7 @@
 function truncate(str, maxlength) {
   let string = str;
   if (str.length > maxlength) {
-    string = str.substr(0, maxlength).slice(0, -1) + "…";
+    string = str.substr(0, maxlength - 1) + "…";
   }
   return string;
 }
