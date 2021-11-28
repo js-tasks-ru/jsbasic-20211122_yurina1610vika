32c058376bd1f51b8d799f498c2ff97de97c5739
Victoria Iurina <Victoria_iurina@epam.com>
2021-11-29 00:51:34 +0300

    Added truncate task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,7 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let string = str;
+  if (str.length > maxlength) {
+    string = str.substr(0, maxlength).slice(0, -1) + "…";
+  }
+  return string;
 }
