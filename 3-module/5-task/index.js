8c187fe4caffe64ed11b057e3df7b19304f516da
Victoria Iurina <Victoria_iurina@epam.com>
2021-12-05 17:04:08 +0300

    Added getMinMax task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,18 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let newArr = [];
+
+  arr.map(function (item) {
+    if (typeof +item === 'number' && !isNaN(+item)) {
+      newArr.push(+item);
+    }
+  });
+
+  let max = Math.max(...newArr);
+  let min = Math.min(...newArr);
+
+  return {
+    min: min,
+    max: max,
+  };
 }
