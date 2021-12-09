6d3c8dfb39366ebf18912f4fa29760b6d6975a05
Victoria Iurina <Victoria_iurina@epam.com>
2021-12-09 16:46:31 +0300

    Fixed format highlight task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,28 +1,26 @@
 function highlight(table) {
   let rows = table.rows;
   for (let i = 1; i < rows.length; i++) {
-    if(rows[i].cells[3].hasAttribute('data-available')) {
-      if(rows[i].cells[3].getAttribute('data-available') === 'true') {
+    if (rows[i].cells[3].hasAttribute('data-available')) {
+      if (rows[i].cells[3].getAttribute('data-available') === 'true') {
         rows[i].classList.add('available');
       } else {
         rows[i].classList.add('unavailable');
       }
-      console.log(rows[i].cells[3].getAttribute('data-available')) ;
-
     } else {
       rows[i].hidden = true;
     }
   }
   for (let i = 1; i < rows.length; i++) {
-    if(rows[i].cells[2].textContent === 'm') {
+    if (rows[i].cells[2].textContent === 'm') {
       rows[i].classList.add('male');
     }
-    if(rows[i].cells[2].textContent === 'f') {
+    if (rows[i].cells[2].textContent === 'f') {
       rows[i].classList.add('female');
     }
   }
   for (let i = 1; i < rows.length; i++) {
-    if(+(rows[i].cells[1].textContent) < 18) {
+    if (+(rows[i].cells[1].textContent) < 18) {
       rows[i].style.textDecoration = 'line-through';
     }
   }
