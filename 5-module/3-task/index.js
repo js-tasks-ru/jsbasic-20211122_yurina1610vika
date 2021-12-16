04465c30c72a6232db75ab479da9dd89712668bf
Victoria Iurina <Victoria_iurina@epam.com>
2021-12-17 01:00:17 +0300

    Added initCarousel task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const disableRightPosition = carouselSlideWidth * 3;
+  const disableLeftPosition = 0;
+  let position = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () => {
+    position -= carouselSlideWidth;
+    carouselInner.style.transform = `translateX(${position}px)`;
+    if (position === -disableRightPosition) {
+      arrowRight.style.display = 'none';
+    }
+    if (position !== disableLeftPosition) {
+      arrowLeft.style.display = 'flex';
+    }
+  });
+
+  arrowLeft.addEventListener('click', () => {
+    position += carouselSlideWidth;
+    carouselInner.style.transform = `translateX(${position}px)`;
+    if (position === disableLeftPosition) {
+      arrowLeft.style.display = 'none';
+    }
+    if (position !== -disableRightPosition) {
+      arrowRight.style.display = 'flex';
+    }
+  });
 }
