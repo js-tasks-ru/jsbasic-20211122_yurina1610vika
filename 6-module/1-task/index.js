233eefd9b86a021ceb5c317ba3d5278a6fd4ca5e
Victoria Iurina <Victoria_iurina@epam.com>
2021-12-21 23:05:57 +0300

    Added UserTable task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,30 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+  }
+  render() {
+    const table = document.createElement('TABLE');
+    let tbody = document.createElement('TBODY');
+    let thead = document.createElement('THEAD');
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    thead.insertAdjacentHTML('afterbegin', `<tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr>`);
+    tbody.insertAdjacentHTML('afterbegin', this.rows.map(item => {
+      return `<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td><td><button class="btn">Х</button></td></tr>`;
+    }).join(''));
+
+    tbody.addEventListener('click', this.onClick);
+    return table;
+  }
+
+  onClick(event) {
+    if (event.target.tagName === 'BUTTON') {
+      let row = event.target.closest('TR');
+      row.parentElement.removeChild(row);
+    }
   }
 }
